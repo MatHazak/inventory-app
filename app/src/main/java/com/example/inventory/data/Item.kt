c35b22e877ad614fe14baac09b7759ae163ac410
MatHazak <mathazak@gmail.com>
2023-08-10 23:13:17 +0330

    Implement Room database

---
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -0,0 +1,12 @@
+package com.example.inventory.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "item_table")
+data class Item (
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val name: String,
+    val price: Double,
+    val quantity: Int,
+)
